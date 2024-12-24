8ec4d6d7e601929f6d7b27c5ef20f8c831589f9a
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-12-25 01:03:56 +0530

    new: counter

--- src/app/(projects)/counter/page.js
+++ src/app/(projects)/counter/page.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+'use client';
+
+import React, { useState } from 'react';
+import { VStack, Heading, HStack } from '@chakra-ui/react';
+import { Button } from '@/components/ui/button';
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
+    <VStack>
       <Heading size='5xl'>Counter</Heading>
-    </div>
+      <HStack>
+        <Button colorPalette='red' onClick={() => setCount((prev) => prev - 1)}>
+          -
+        </Button>
+        <Heading size='3xl'>{count}</Heading>
+        <Button
+          colorPalette='green'
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          +
+        </Button>
+      </HStack>
+    </VStack>
   );
 };
 
