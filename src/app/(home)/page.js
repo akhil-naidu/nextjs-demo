9aed17c5f2369e9b760dc9f88616563caf7c72a8
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-12-25 00:32:51 +0530

    init: basic setup without styles

---
+++ src/app/(home)/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hi, I'm Akhil Naidu</h1>
+
+      <p>
+        These are my projects, use the navigation to navigate to that project
+      </p>
+    </div>
+  );
+};
+
+export default Home;
